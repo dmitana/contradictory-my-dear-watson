703934e0cd8b55362ef9ed19d05f2e8645b33d0d
Denis Mitana <denis.mitana1@gmail.com>
2020-08-30 16:51:19 +0200

    Add arguments for train command

--- contradictory_my_dear_watson/cli/arguments.py
+++ contradictory_my_dear_watson/cli/arguments.py
@@ -13,7 +13,8 @@ def create_argument_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog='contradictory-my-dear-watson',
         description='Program to solve the Contradictory, My Dear Watson '
-                    'problem.'
+                    'problem.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
     # Subparsers
@@ -36,8 +37,86 @@ def create_train_subparser(subparsers: argparse._SubParsersAction) -> None:
         added.
     """
     parser_train = subparsers.add_parser(
-        'train', help='train a new model'
+        'train',
+        help='train a new model',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
+
+    # Required named arguments
+    parser_required_named = parser_train.add_argument_group(
+        'required named arguments'
+    )
+    parser_required_named.add_argument(
+        '--train-data-path',
+        required=True,
+        type=str,
+        help='Path to the training dataset.'
+
+    )
+
+    # Hyperparameters agruments
+    parser_hparams = parser_train.add_argument_group(
+        'hyperparameters'
+    )
+    parser_hparams.add_argument(
+        '--lstm-hidden-size',
+        type=int,
+        default=256,
+        help='Number of neurons in a LSTM layer.'
+    )
+    parser_hparams.add_argument(
+        '--learning-rate',
+        type=float,
+        default=0.01,
+        help='Learning rate.'
+    )
+    parser_hparams.add_argument(
+        '--tokenizer',
+        type=str,
+        default='spacy',
+        help='Tokenizer name to be used by PyTorch `get_tokenizer`'
+             'function to get instance of `Tokenizer`.'
+    )
+    parser_hparams.add_argument(
+        '--tokenizer-language',
+        type=str,
+        default='en_core_web_sm',
+        help='Tokenizer language to be used by PyTorch `get_tokenizer`'
+             'function to get instance of `Tokenizer`.'
+    )
+    # TODO: add choices
+    parser_hparams.add_argument(
+        '--vectors',
+        type=str,
+        default='fasttext.simple.300d',
+        help='Vectors name to be loaded by PyTorch `load_vectors` method.'
+    )
+    parser_hparams.add_argument(
+        '--batch-size',
+        type=int,
+        default=1,
+        help='Size of one mini-batch.'
+    )
+    parser_hparams.add_argument(
+        '--epochs',
+        type=int,
+        default=1,
+        help='Number of training epochs.'
+    )
+    parser_hparams.add_argument(
+        '--num-workers',
+        type=int,
+        default=0,
+        help='How many subprocesses to use for data loading. `0` means '
+             'that the data will be loaded in the main process.'
+    )
+    parser_hparams.add_argument(
+        '--print-summary',
+        action='store_true',
+        help='Whether to print model summary.'
+    )
+
+    # Add execution function
     parser_train.set_defaults(func=train)
 
 
