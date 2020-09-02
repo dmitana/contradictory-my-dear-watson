5cd68c3e82c0120020b9254455bb88c9fc528e8e
Denis Mitana <denis.mitana1@gmail.com>
2020-09-02 18:52:33 +0200

    Add dataset command

---
+++ contradictory_my_dear_watson/cli/dataset.py
@@ -0,0 +1,16 @@
+from argparse import Namespace
+
+
+def dataset(args: Namespace) -> None:
+    """
+    Create train, dev and test dataset.
+
+    :param args: dataset command's arguments.
+    """
+    args.dataset(
+        args.data_path,
+        args.train_size,
+        args.dev_size,
+        args.test_size,
+        args.sanity_size
+    )()
--- contradictory_my_dear_watson/cli/parser.py
+++ contradictory_my_dear_watson/cli/parser.py
@@ -2,8 +2,12 @@ import argparse
 import logging
 from typing import Any, Callable, Dict, List, Optional
 
+from contradictory_my_dear_watson.cli.dataset import dataset
 from contradictory_my_dear_watson.cli.evaluate import evaluate
 from contradictory_my_dear_watson.cli.train import train
+from contradictory_my_dear_watson.datasets import (
+    ContradictoryMyDearWatsonDataset
+)
 from contradictory_my_dear_watson.models import BiLSTMModel
 
 
@@ -66,6 +70,7 @@ def create_argument_parser() -> argparse.ArgumentParser:
     )
     create_train_subparser(subparsers, [parent_parser])
     create_evaluate_subparser(subparsers, [parent_parser])
+    create_dataset_subparser(subparsers, [parent_parser])
 
     return parser
 
@@ -90,6 +95,12 @@ def create_train_subparser(
 
     # Optional arguments
     parser_train.add_argument(
+        '--dev-data-path',
+        type=str,
+        default=None,
+        help='Path to the development dataset.'
+    )
+    parser_train.add_argument(
         '--num-workers',
         type=int,
         default=0,
@@ -111,7 +122,6 @@ def create_train_subparser(
         required=True,
         type=str,
         help='Path to the training dataset.'
-
     )
 
     # Hyperparameters
@@ -194,6 +204,77 @@ def create_evaluate_subparser(
     parser_evaluate.set_defaults(func=evaluate)
 
 
+def create_dataset_subparser(
+    subparsers: argparse._SubParsersAction,
+    parents: Optional[List[argparse.ArgumentParser]] = None
+) -> None:
+    """
+    Create parser for the `dataset` command.
+
+    :param subparsers: argparse subparsers where a new parser will be
+        added.
+    :param parents: subparser parents.
+    """
+    parser_dataset = subparsers.add_parser(
+        'dataset',
+        parents=[] if parents is None else parents,
+        help='create train, dev and test dataset',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    # Required named arguments
+    parser_required_named = parser_dataset.add_argument_group(
+        'required named arguments'
+    )
+    parser_required_named.add_argument(
+        '--dataset',
+        required=True,
+        type=arg_map(**dataset_mapping),
+        metavar=f'{{{",".join(key for key in dataset_mapping.keys())}}}',
+        help='Name of dataset for appropriate preprocess function.'
+    )
+    parser_required_named.add_argument(
+        '--data-path',
+        required=True,
+        type=str,
+        help='Path to the data.'
+    )
+
+    # Dataset specification arguments
+    parser_dataset_specs = parser_dataset.add_argument_group(
+        'dataset specification arguments'
+    )
+    parser_dataset_specs.add_argument(
+        '--train-size',
+        type=int,
+        default=80,
+        help='Size of train dataset in percentage.'
+    )
+    parser_dataset_specs.add_argument(
+        '--dev-size',
+        type=int,
+        default=10,
+        help='Size of development dataset in percentage.'
+    )
+    parser_dataset_specs.add_argument(
+        '--test-size',
+        type=int,
+        default=10,
+        help='Size of test dataset in percentage.'
+    )
+    parser_dataset_specs.add_argument(
+        '--sanity-size',
+        type=int,
+        default=0,
+        help='If greater than `0` then create dataset for sanity check '
+             'with `--sanity-size` elements. Ignore `--train-size`, '
+             '`--dev-size` and `--test-size` if set.'
+    )
+
+    # Add execution function
+    parser_dataset.set_defaults(func=dataset)
+
+
 # Define mappings
 logging_level_mapping = {
     'critical': logging.CRITICAL,
@@ -205,4 +286,8 @@ logging_level_mapping = {
 
 model_mapping = {
     'baseline': BiLSTMModel
+}
+
+dataset_mapping = {
+    'contradictory-my-dear-watson': ContradictoryMyDearWatsonDataset
 }
